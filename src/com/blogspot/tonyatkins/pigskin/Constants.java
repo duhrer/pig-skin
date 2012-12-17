17975869bbada9150d790f49a1eb458bf4108aff
Tony Atkins <duhrer@gmail.com>
2012-12-18 00:14:29 +0800

    Converted to using lucene (which required converting to maven android plugin).

--- src/com/blogspot/tonyatkins/pigskin/Constants.java
+++ src/com/blogspot/tonyatkins/pigskin/Constants.java
@@ -1,5 +1,6 @@
 package com.blogspot.tonyatkins.pigskin;
 
+
 public class Constants {
 	// Preference keys
 	public static final String FULL_SCREEN_PREF = "fullScreen";
@@ -8,4 +9,11 @@ public class Constants {
 	// Defaults
 	public static final boolean DEFAULT_FULL_SCREEN=true;
 	public static final String DEFAULT_DICTIONARY= "sowpods";
+	
+	public final static String[] DICTIONARIES = new String[] { "sowpods", "opentaal"};
+	public final static String DICTIONARY_ROOT_FILESYSTEM_PATH = "/Users/aatkins/Source/pig-skin/extras/dictionary";
+	public final static String DICTIONARY_INDEX_ROOT_FILESYSTEM_PATH = "/Users/aatkins/Source/pig-skin/assets/dictionary/indexes";
+	public static final String SEARCH_FIELD = "word";
+	public static final int MAX_SEARCH_RESULTS = 100;
+	public static final int BUFFER_SIZE = 4096;
 }
